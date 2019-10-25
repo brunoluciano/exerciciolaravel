fab12818243c073221cb4411656133a5b5223290
brunoluciano <bruno.lucian0544@gmail.com>
2019-10-25 19:37:49 -0300

    Criação Migration Controller funcionário

---
+++ resources/views/funcionarios/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron bg-dark text-white py-4">
+                <h1 class="display-3">Funcionários</h1>
+                <hr class="my-2 bg-info">
+                <br>
+                <table class="table table-info">
+                    <thead class="bg-primary text-white">
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>CARGO</th>
+                            <th>DATA MATRÍCULA</th>
+                            <th>SALÁRIO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row"></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('funcionarios/index');
 });
+
+Route::resource('funcionarios', 'FuncionarioController');
